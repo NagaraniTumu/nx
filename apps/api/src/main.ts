f4aecea9b050116186def8dc66a16ded1dddc1f0
Nagarani Tumu <tnagarani@pkglobal.com>
2021-06-11 14:05:23 +0530

    first commit

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,26 @@
 import * as express from 'express';
+
 import { Message } from '@bghoard/api-interfaces';
+
+import { getGames } from './app/game';
+import { createReview, getReviews } from './app/review';
+import { addItemToCart, getCart, updateItemInCart } from './app/store';
 
 const app = express();
 
-const greeting: Message = { message: 'Welcome to api!' };
-
 app.get('/api', (req, res) => {
+  const greeting: Message = { message: 'Welcome to api!' };
   res.send(greeting);
 });
+
+app.get('/api/game', getGames);
+
+app.get('/api/review/:game', getReviews);
+app.get('/api/review/:game', createReview);
+
+app.get('/api/cart', getCart);
+app.get('/api/cart', addItemToCart);
+app.get('/api/cart', updateItemInCart);
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
